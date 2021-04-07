0951b82c32c4f06298af4a7f8b62189909469a96
Tom Killingback <tomkillingback@gmail.com>
2021-04-07 18:01:22 +1000

    add more week 5

---
+++ week5/scalar_multiply.c
@@ -0,0 +1,10 @@
+// COMP1511 Week 5 tut question
+// Write a function that takes in a 2D array of ints and 
+// multiplies every value in the array by a given int.
+
+void scalar_multiply(int rows, int columns, int matrix[rows][columns],  int scalar);
+
+int main(void) {
+
+    return 0;
+}
