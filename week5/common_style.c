136c062617581199ed414a28e1dc2b1e96b29727
Tom Killingback <tomkillingback@gmail.com>
2021-07-01 13:09:13 +1000

    update assignment tips

--- week5/common_style.c
+++ week5/common_style.c
@@ -12,8 +12,10 @@
     - #defines
         - No 'magic numbers' in your code
         - give all constants meaning
+        - no "while (counter < 5)" <-- 5 should have a meaning & #define
     
     - Use of funtions
+        - Don't 'over-nest' your control structures
         - No longer than 100 lines
         - Please keep around 50-60
         - Best is ~40
@@ -21,5 +23,8 @@
     - Comments
         - explain what your code does, not how it does 
         - brief and descriptive of sections
+        - as if someone has never read the assignment spec
+
+    Start Early & Get help!
 
 */
