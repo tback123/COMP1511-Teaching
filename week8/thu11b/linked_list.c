68162ab1ff5545333b93c8f8ba311503c1768ca9
Tom Killingback <tomkillingback@gmail.com>
2021-07-22 12:50:06 +1000

    add thu11b code

--- week8/thu11b/linked_list.c
+++ week8/thu11b/linked_list.c
@@ -10,10 +10,11 @@ struct student {
     char name[MAX_LENGTH];
     int zid;
     int assign1;
+    struct student *next_stu;
 };
 
 struct student *create_student(int zid, int assign1_mark, char name[MAX_LENGTH]);
-
+void print_list(struct student *first_student);
 void change_mark(struct student *the_student, int new_mark);
 void print_student(struct student *a_student);
 
@@ -21,20 +22,32 @@ int main(void) {
 
     struct student *stu1 = create_student(5256086, 41, "Tom");
     struct student *stu2 = create_student(5125843, 98, "Lucca");
+    struct student *stu3 = create_student(1234567, 100, "Cat");
 
-    printf("Before updating mark:");
-    print_student(stu1);
-    print_student(stu2);
+    stu1->next_stu = stu2;
+    stu2->next_stu = stu3;
 
-    printf("########\n");
-
-    change_mark(stu1, 82);
-    print_student(stu1);
-    print_student(stu2);
+    print_list(stu1);
 
     return 0;
 
 }
+
+void print_list(struct student *first_student) {
+
+    struct student *curr_student = first_student;
+    while (curr_student != NULL) {
+
+        // Do some code with the current studnet
+        print_student(curr_student);
+
+        // Move to the next student
+        curr_student = curr_student->next_stu;
+
+    }
+
+}
+
 
 struct student *create_student(int zid, int assign1_mark, char name[MAX_LENGTH]) {
 
@@ -42,6 +55,7 @@ struct student *create_student(int zid, int assign1_mark, char name[MAX_LENGTH])
     new_student->zid = zid;
     new_student->assign1 = assign1_mark;
     strcpy(new_student->name, name);
+    new_student->next_stu = NULL;
 
     return new_student;
 }
--- week8/thu11b/struct.c
+++ week8/thu11b/struct.c
@@ -6,11 +6,40 @@
 
 #define MAX_LENGTH 100
 
+struct student {
+    char name[MAX_LENGTH];
+    int zid;
+    int assign1;
+};
+
+void change_mark(struct student the_student, int new_mark);
 void print_student(struct student a_student);
-void change_mark(struct student the_student, double new_mark);
 
 int main(void) {
-    
+
+    struct student student1;
+    student1.zid = 5256086;
+    student1.assign1 = 41;
+    strcpy(student1.name, "Tom");
+
+    printf("Before updating mark:");
+    print_student(student1);
+
+    change_mark(student1, 82);
+    print_student(student1);
+
+}
+
+void change_mark(struct student the_student, int new_mark) {
+    the_student.assign1 = new_mark;
+}
+
+void print_student(struct student a_student) {
+
+    printf("The student has:\n");
+    printf("zID: %d\n", a_student.zid);
+    printf("assign1: %d\n", a_student.assign1);
+    printf("name: %s\n\n", a_student.name);
 
 }
 
@@ -45,8 +74,3 @@ int main(void) {
 
 
 
-
-
-void print_student(struct student a_student){
-    printf("%s with zID z%d has an assignment1 mark of %f\n", a_student.name, a_student.zid, a_student.ass1_mark);
-}
