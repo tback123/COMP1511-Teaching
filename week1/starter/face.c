e04011873518598d0b3bdd0fdecb0ad26dc4bfbf
Tom Killingback <tomkillingback@gmail.com>
2021-09-15 12:49:41 +1000

    update file structure for 21T3

--- week1/starter/face.c
+++ week1/starter/face.c
@@ -1,11 +1,15 @@
+d// This prints out a face
+// Tom
+
 #include <stdio.h>
 
 int main (void) {
 
     printf("~ ~\n");
     printf("0 0\n");
-    printf(" o\n");
-    printf(" -\n");
+    printf(" o \n");
+    printf(" - \n");
+    printf("\n");
     
     return 0;
 }
--- week1/starter/hello_world.c
+++ week1/starter/hello_world.c
@@ -5,7 +5,7 @@
 
 int main(void) {
 
-    printf("Hello World!");
+    printf("Hello World!\n");
 
     return 0;
 }
