26c6f17bbe6d338c854e67be659a36cfc76a0db4
Tom Killingback <tomkillingback@gmail.com>
2022-09-14 19:08:42 +1000

    22T3 wed17c wk1

---
+++ week_1/22T3/wed17c/hello.c
@@ -0,0 +1,24 @@
+// Initial Hello World Program
+// Written by W17C on 14/09/22
+
+/*
+     /\___/\
+    (=' w '=)
+
+
+    anything in here is ignored!
+
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+    // Do stuff here!
+    printf("Hello World!\n");
+
+    printf(" /\\___/\\\n");
+    printf("(=' w '=)\n");
+
+    return 0;
+}
